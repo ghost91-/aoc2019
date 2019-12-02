04aa9110f07b1e2e9877bb662243cef30e8ff1c4
Johannes Loher <johannes.loher@fg4f.de>
2019-12-02 22:16:43 +0100

    add haskell version of day 1 part 1

---
+++ day1/part1/main.hs
@@ -0,0 +1,9 @@
+main = do
+    contents <- readFile "input"
+    print . sum . map calculateRequiredFuel. map readInt . lines $ contents
+
+calculateRequiredFuel :: Integral a => a -> a
+calculateRequiredFuel x = x `div` 3 - 2
+
+readInt :: String -> Int
+readInt = read
