90a9a6ba1b9480aa2abb15806bc2ec2a7fb8f7f6
Johannes Loher <johannes.loher@fg4f.de>
2019-12-07 19:34:18 +0100

    improve day 7 part 1

--- day7/part1/main.d
+++ day7/part1/main.d
@@ -2,315 +2,441 @@ module day7.part1.main;
 
 import std;
 
-struct LastOutput
+auto outputRange(alias fn)()
 {
-private:
-    int current;
-
-public:
-    void put(int val)
+    struct R
     {
-        current = val;
+        void put(Parameters!fn e)
+        {
+            fn(e);
+        }
     }
 
-    int get()
-    {
-        return current;
-    }
+    return R();
 }
 
 void main()
 {
     auto memory = File("input", "r").readln.strip.splitter(",").map!(to!int).array;
 
-    iota(5).permutations
+    maxThrusterSignal(memory).writeln;
+}
+
+auto maxThrusterSignal(int[] memory)
+{
+    return iota(5).permutations
         .map!(phaseSettings => phaseSettings.fold!((input, phaseSetting) {
-                LastOutput or;
-                auto ir = [phaseSetting, input];
-                executeProgram!(ir, or)(memory.dup);
-                return or.get;
+                int result;
+                execute(memory.dup, [phaseSetting, input], outputRange!((int i) {
+                    result = i;
+                })());
+                return result;
             })(0))
-        .fold!max
-        .writeln;
+        .fold!max;
 }
 
-enum Opcode
+unittest
 {
-    add = 1,
-    multiply = 2,
-    input = 3,
-    output = 4,
-    jumpIfTrue = 5,
-    jumpIfFalse = 6,
-    lessThan = 7,
-    equals = 8,
-    halt = 99
+    //given
+    auto memory = [
+        3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0
+    ];
+
+    // when
+    immutable result = maxThrusterSignal(memory);
+
+    // then
+    assert(result == 43_210);
 }
 
-enum ParameterMode
+unittest
 {
-    position = 0,
-    immediate = 1
+    //given
+    auto memory = [
+        3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1,
+        24, 23, 23, 4, 23, 99, 0, 0
+    ];
+
+    // when
+    immutable result = maxThrusterSignal(memory);
+
+    // then
+    assert(result == 54_321);
 }
 
-struct Parameter
+unittest
 {
-    int value;
-    ParameterMode mode;
+    //given
+    auto memory = [
+        3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
+        1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0
+    ];
 
-    ref int derefIfNeeded(int[] memory)
-    {
-        if (mode == ParameterMode.immediate)
-            return value;
-        else
-            return memory[value];
-    }
+    // when
+    immutable result = maxThrusterSignal(memory);
+
+    // then
+    assert(result == 65_210);
 }
 
 alias Continue = Flag!"continue";
 
-auto parameterMode(int value, size_t parameterIndex)
+auto mode(const int value, const size_t paramIdx)
 {
-    immutable factor = 100 * 10 ^^ parameterIndex;
-    immutable parameterMode = (value / factor) % 10;
-    static foreach (member; EnumMembers!ParameterMode)
-    {
-        if (member == parameterMode)
-            return member;
-    }
-    throw new Exception("Invalid parameter mode %s".format(parameterMode));
+    return (value / (100 * 10 ^^ paramIdx)) % 10;
 }
 
-auto parameter(const size_t instructionPointer, const int[] memory, const size_t parameterIndex)
+ref int param(const size_t ip, int[] memory, const size_t paramIdx)
 {
-    auto mode = parameterMode(memory[instructionPointer], parameterIndex);
-    return Parameter(memory[instructionPointer + 1 + parameterIndex], mode);
+    return mode(memory[ip], paramIdx) == 0 ? memory[memory[ip + 1 + paramIdx]]
+        : memory[ip + 1 + paramIdx];
 }
 
-interface Instruction
-{
-    Continue execute(ref size_t instructionPointer, int[] memory);
-}
+alias addInstruction = (ref size_t ip, int[] memory) => ({
+    param(ip, memory, 2) = param(ip, memory, 0) + param(ip, memory, 1);
+    ip += 4;
+    return Continue.yes;
+});
 
-class AddInstruction : Instruction
-{
-private:
-    Parameter value1, value2, outAddress;
+alias multiplyInstruction = (ref size_t ip, int[] memory) => ({
+    param(ip, memory, 2) = param(ip, memory, 0) * param(ip, memory, 1);
+    ip += 4;
+    return Continue.yes;
+});
 
-public:
-    this(size_t instructionPointer, int[] memory)
-    {
-        value1 = parameter(instructionPointer, memory, 0);
-        value2 = parameter(instructionPointer, memory, 1);
-        outAddress = parameter(instructionPointer, memory, 2);
-    }
+alias inputInstruction = (ref size_t ip, int[] memory, ref input) => ({
+    param(ip, memory, 0) = input.front;
+    input.popFront();
+    ip += 2;
+    return Continue.yes;
+});
 
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        outAddress.derefIfNeeded(memory) = value1.derefIfNeeded(
-                memory) + value2.derefIfNeeded(memory);
-        instructionPointer += 4;
-        return Continue.yes;
-    }
-}
+alias outputInstruction = (ref size_t ip, int[] memory, ref output) => ({
+    output.put(param(ip, memory, 0));
+    ip += 2;
+    return Continue.yes;
+});
 
-class MultiplyInstruction : Instruction
-{
-private:
-    Parameter value1, value2, outAddress;
+alias jumpIfTrueInstruction = (ref size_t ip, int[] memory) => ({
+    if (param(ip, memory, 0) != 0)
+        ip = param(ip, memory, 1).to!size_t;
+    else
+        ip += 3;
+    return Continue.yes;
+});
 
-public:
-    this(size_t instructionPointer, int[] memory)
-    {
-        value1 = parameter(instructionPointer, memory, 0);
-        value2 = parameter(instructionPointer, memory, 1);
-        outAddress = parameter(instructionPointer, memory, 2);
-    }
+alias jumpIfFalseInstruction = (ref size_t ip, int[] memory) => ({
+    if (param(ip, memory, 0) == 0)
+        ip = param(ip, memory, 1).to!size_t;
+    else
+        ip += 3;
+    return Continue.yes;
+});
 
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        outAddress.derefIfNeeded(memory) = value1.derefIfNeeded(
-                memory) * value2.derefIfNeeded(memory);
-        instructionPointer += 4;
-        return Continue.yes;
-    }
-}
+alias lessThanInstruction = (ref size_t ip, int[] memory) => ({
+    param(ip, memory, 2) = param(ip, memory, 0) < param(ip, memory, 1) ? 1 : 0;
+    ip += 4;
+    return Continue.yes;
+});
 
-class InputInstruction(alias ir) : Instruction
-{
-private:
-    Parameter outAddress;
+alias equalsInstruction = (ref size_t ip, int[] memory) => ({
+    param(ip, memory, 2) = param(ip, memory, 0) == param(ip, memory, 1) ? 1 : 0;
+    ip += 4;
+    return Continue.yes;
+});
 
-public:
-    this(size_t instructionPointer, int[] memory)
-    {
-        outAddress = parameter(instructionPointer, memory, 0);
-    }
+alias haltInstruction = (ref size_t ip, int[] memory) => delegate() => Continue.no;
 
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        outAddress.derefIfNeeded(memory) = ir.front;
-        ir.popFront();
-        instructionPointer += 2;
-        return Continue.yes;
-    }
-}
-
-class OutputInstruction(alias or) : Instruction
-{
-private:
-    Parameter value;
-
-public:
-    this(size_t instructionPointer, int[] memory)
-    {
-        value = parameter(instructionPointer, memory, 0);
-    }
-
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        or.put(value.derefIfNeeded(memory));
-        instructionPointer += 2;
-        return Continue.yes;
-    }
-}
-
-class JumpIfTrueInstruction : Instruction
-{
-private:
-    Parameter toBeChecked, target;
-
-public:
-    this(size_t instructionPointer, int[] memory)
-    {
-        toBeChecked = parameter(instructionPointer, memory, 0);
-        target = parameter(instructionPointer, memory, 1);
-    }
-
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        if (toBeChecked.derefIfNeeded(memory) != 0)
-            instructionPointer = target.derefIfNeeded(memory).to!size_t;
-        else
-            instructionPointer += 3;
-        return Continue.yes;
-    }
-}
-
-class JumpIfFalseInstruction : Instruction
-{
-private:
-    Parameter toBeChecked, target;
-
-public:
-    this(size_t instructionPointer, int[] memory)
-    {
-        toBeChecked = parameter(instructionPointer, memory, 0);
-        target = parameter(instructionPointer, memory, 1);
-    }
-
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        if (toBeChecked.derefIfNeeded(memory) == 0)
-            instructionPointer = target.derefIfNeeded(memory).to!size_t;
-        else
-            instructionPointer += 3;
-        return Continue.yes;
-    }
-}
-
-class LessThanInstruction : Instruction
-{
-private:
-    Parameter value1, value2, outAddress;
-
-public:
-    this(size_t instructionPointer, int[] memory)
-    {
-        value1 = parameter(instructionPointer, memory, 0);
-        value2 = parameter(instructionPointer, memory, 1);
-        outAddress = parameter(instructionPointer, memory, 2);
-    }
-
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        outAddress.derefIfNeeded(memory) = value1.derefIfNeeded(
-                memory) < value2.derefIfNeeded(memory) ? 1 : 0;
-        instructionPointer += 4;
-        return Continue.yes;
-    }
-}
-
-class EqualsInstruction : Instruction
-{
-private:
-    Parameter value1, value2, outAddress;
-
-public:
-    this(size_t instructionPointer, int[] memory)
-    {
-        value1 = parameter(instructionPointer, memory, 0);
-        value2 = parameter(instructionPointer, memory, 1);
-        outAddress = parameter(instructionPointer, memory, 2);
-    }
-
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        outAddress.derefIfNeeded(memory) = value1.derefIfNeeded(
-                memory) == value2.derefIfNeeded(memory) ? 1 : 0;
-        instructionPointer += 4;
-        return Continue.yes;
-    }
-}
-
-class HaltInstruction : Instruction
-{
-    this(size_t instructionPointer, int[] memory)
-    {
-    }
-
-    override Continue execute(ref size_t instructionPointer, int[] memory)
-    {
-        return Continue.no;
-    }
-}
-
-Instruction nextInstruction(alias ir, alias or)(size_t instructionPointer, int[] memory)
-{
-    immutable opcode = memory[instructionPointer] % 100;
-    switch (opcode)
-    {
-    case Opcode.add:
-        return new AddInstruction(instructionPointer, memory);
-    case Opcode.multiply:
-        return new MultiplyInstruction(instructionPointer, memory);
-    case Opcode.input:
-        return new InputInstruction!(ir)(instructionPointer, memory);
-    case Opcode.output:
-        return new OutputInstruction!(or)(instructionPointer, memory);
-    case Opcode.jumpIfTrue:
-        return new JumpIfTrueInstruction(instructionPointer, memory);
-    case Opcode.jumpIfFalse:
-        return new JumpIfFalseInstruction(instructionPointer, memory);
-    case Opcode.lessThan:
-        return new LessThanInstruction(instructionPointer, memory);
-    case Opcode.equals:
-        return new EqualsInstruction(instructionPointer, memory);
-    case Opcode.halt:
-        return new HaltInstruction(instructionPointer, memory);
-    default:
-        throw new Exception("Invalid opcode %s".format(opcode));
-    }
-}
-
-void executeProgram(alias ir, alias or)(int[] memory)
+void execute(Input, Output)(int[] memory, Input input, Output output)
 {
     size_t instructionPointer = 0;
     while (true)
     {
-        auto instruction = nextInstruction!(ir, or)(instructionPointer, memory);
-        immutable _continue = instruction.execute(instructionPointer, memory);
+        immutable opcode = memory[instructionPointer] % 100;
+
+        immutable _continue = [
+            1: addInstruction(instructionPointer, memory),
+            2: multiplyInstruction(instructionPointer, memory),
+            3: inputInstruction(instructionPointer, memory, input),
+            4: outputInstruction(instructionPointer, memory, output),
+            5: jumpIfTrueInstruction(instructionPointer, memory),
+            6: jumpIfFalseInstruction(instructionPointer, memory),
+            7: lessThanInstruction(instructionPointer, memory),
+            8: equalsInstruction(instructionPointer, memory),
+            99: haltInstruction(instructionPointer, memory),
+        ][opcode]();
         if (_continue == Continue.no)
             break;
+    }
+}
+
+@("basic math")
+{
+    unittest
+    {
+        // given
+        auto memory = [1, 0, 0, 0, 99];
+        int[] input = [];
+
+        // when
+        execute(memory, input, nullSink);
+
+        assert(memory == [2, 0, 0, 0, 99]);
+    }
+
+    unittest
+    {
+        // given
+        auto memory = [2, 3, 0, 3, 99];
+        int[] input = [];
+
+        // when
+        execute(memory, input, nullSink);
+
+        assert(memory == [2, 3, 0, 6, 99]);
+    }
+
+    unittest
+    {
+        // given
+        auto memory = [2, 4, 4, 5, 99, 0];
+        int[] input = [];
+
+        // when
+        execute(memory, input, nullSink);
+
+        assert(memory == [2, 4, 4, 5, 99, 9801]);
+    }
+
+    unittest
+    {
+        // given
+        auto memory = [1, 1, 1, 4, 99, 5, 6, 0, 99];
+        int[] input = [];
+
+        // when
+        execute(memory, input, nullSink);
+
+        assert(memory == [30, 1, 1, 4, 2, 5, 6, 0, 99]);
+    }
+
+    unittest
+    {
+        // given
+        auto memory = [
+            1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 6, 1, 19, 1,
+            19, 5, 23, 2, 9, 23, 27, 1, 5, 27, 31, 1, 5, 31, 35, 1, 35, 13,
+            39, 1, 39, 9, 43, 1, 5, 43, 47, 1, 47, 6, 51, 1, 51, 13, 55, 1, 55,
+            9, 59, 1, 59, 13, 63, 2, 63, 13, 67, 1, 67, 10, 71, 1, 71, 6, 75,
+            2, 10, 75, 79, 2, 10, 79, 83, 1, 5, 83, 87, 2, 6, 87, 91, 1, 91, 6,
+            95, 1, 95, 13, 99, 2, 99, 13, 103, 1, 103, 9, 107, 1, 10, 107,
+            111, 2, 111, 13, 115, 1, 10, 115, 119, 1, 10, 119, 123, 2, 13,
+            123, 127, 2, 6, 127, 131, 1, 13, 131, 135, 1, 135, 2, 139, 1, 139,
+            6, 0, 99, 2, 0, 14, 0
+        ];
+        int[] input = [];
+
+        // when
+        execute(memory, input, nullSink);
+
+        assert(memory[0] == 4_090_689);
+    }
+
+}
+
+@(
+        "Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).")
+{
+    unittest
+    {
+        // given
+        immutable data = "3,9,8,9,10,9,4,9,99,-1,8";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(8);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 1);
+    }
+
+    unittest
+    {
+        // given
+        immutable data = "3,9,8,9,10,9,4,9,99,-1,8";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(7);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 0);
+    }
+}
+
+@(
+        "Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).")
+{
+    unittest
+    {
+        // given
+        immutable data = "3,9,7,9,10,9,4,9,99,-1,8";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(7);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 1);
+    }
+
+    unittest
+    {
+        // given
+        immutable data = "3,9,7,9,10,9,4,9,99,-1,8";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(8);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 0);
+    }
+}
+
+@(
+        "Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).")
+{
+    unittest
+    {
+        // given
+        immutable data = "3,3,1108,-1,8,3,4,3,99";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(8);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 1);
+    }
+
+    unittest
+    {
+        // given
+        immutable data = "3,3,1108,-1,8,3,4,3,99";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(7);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 0);
+    }
+}
+
+@(
+        "Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).")
+{
+    unittest
+    {
+        // given
+        immutable data = "3,3,1107,-1,8,3,4,3,99";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(7);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 1);
+    }
+
+    unittest
+    {
+        // given
+        immutable data = "3,3,1107,-1,8,3,4,3,99";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(8);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 0);
+    }
+}
+
+@(
+        "Consider whether the input is below 8, equal to 8 or above 8; output 999 (below), 1000 (equal) or 1001 (above)")
+{
+
+    unittest
+    {
+        // given
+        immutable data = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(7);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 999);
+    }
+
+    unittest
+    {
+        // given
+        immutable data = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(8);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 1000);
+    }
+
+    unittest
+    {
+        // given
+        immutable data = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+        auto memory = data.splitter(",").map!(to!int).array;
+        int result;
+        auto input = only(9);
+        auto output = outputRange!((int e) { result = e; })();
+
+        // when
+        execute(memory, input, output);
+
+        // then
+        assert(result == 1001);
     }
 }
