a3258a468214ad608666d22b40ae1557afc6627b
Johannes Loher <johannes.loher@fg4f.de>
2019-12-07 13:18:21 +0100

    day 7 part 1

---
+++ day7/part1/main.d
@@ -0,0 +1,316 @@
+module day7.part1.main;
+
+import std;
+
+struct LastOutput
+{
+private:
+    int current;
+
+public:
+    void put(int val)
+    {
+        current = val;
+    }
+
+    int get()
+    {
+        return current;
+    }
+}
+
+void main()
+{
+    auto memory = File("input", "r").readln.strip.splitter(",").map!(to!int).array;
+
+    iota(5).permutations
+        .map!(phaseSettings => phaseSettings.fold!((input, phaseSetting) {
+                LastOutput or;
+                auto ir = [phaseSetting, input];
+                executeProgram!(ir, or)(memory.dup);
+                return or.get;
+            })(0))
+        .fold!max
+        .writeln;
+}
+
+enum Opcode
+{
+    add = 1,
+    multiply = 2,
+    input = 3,
+    output = 4,
+    jumpIfTrue = 5,
+    jumpIfFalse = 6,
+    lessThan = 7,
+    equals = 8,
+    halt = 99
+}
+
+enum ParameterMode
+{
+    position = 0,
+    immediate = 1
+}
+
+struct Parameter
+{
+    int value;
+    ParameterMode mode;
+
+    ref int derefIfNeeded(int[] memory)
+    {
+        if (mode == ParameterMode.immediate)
+            return value;
+        else
+            return memory[value];
+    }
+}
+
+alias Continue = Flag!"continue";
+
+auto parameterMode(int value, size_t parameterIndex)
+{
+    immutable factor = 100 * 10 ^^ parameterIndex;
+    immutable parameterMode = (value / factor) % 10;
+    static foreach (member; EnumMembers!ParameterMode)
+    {
+        if (member == parameterMode)
+            return member;
+    }
+    throw new Exception("Invalid parameter mode %s".format(parameterMode));
+}
+
+auto parameter(const size_t instructionPointer, const int[] memory, const size_t parameterIndex)
+{
+    auto mode = parameterMode(memory[instructionPointer], parameterIndex);
+    return Parameter(memory[instructionPointer + 1 + parameterIndex], mode);
+}
+
+interface Instruction
+{
+    Continue execute(ref size_t instructionPointer, int[] memory);
+}
+
+class AddInstruction : Instruction
+{
+private:
+    Parameter value1, value2, outAddress;
+
+public:
+    this(size_t instructionPointer, int[] memory)
+    {
+        value1 = parameter(instructionPointer, memory, 0);
+        value2 = parameter(instructionPointer, memory, 1);
+        outAddress = parameter(instructionPointer, memory, 2);
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        outAddress.derefIfNeeded(memory) = value1.derefIfNeeded(
+                memory) + value2.derefIfNeeded(memory);
+        instructionPointer += 4;
+        return Continue.yes;
+    }
+}
+
+class MultiplyInstruction : Instruction
+{
+private:
+    Parameter value1, value2, outAddress;
+
+public:
+    this(size_t instructionPointer, int[] memory)
+    {
+        value1 = parameter(instructionPointer, memory, 0);
+        value2 = parameter(instructionPointer, memory, 1);
+        outAddress = parameter(instructionPointer, memory, 2);
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        outAddress.derefIfNeeded(memory) = value1.derefIfNeeded(
+                memory) * value2.derefIfNeeded(memory);
+        instructionPointer += 4;
+        return Continue.yes;
+    }
+}
+
+class InputInstruction(alias ir) : Instruction
+{
+private:
+    Parameter outAddress;
+
+public:
+    this(size_t instructionPointer, int[] memory)
+    {
+        outAddress = parameter(instructionPointer, memory, 0);
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        outAddress.derefIfNeeded(memory) = ir.front;
+        ir.popFront();
+        instructionPointer += 2;
+        return Continue.yes;
+    }
+}
+
+class OutputInstruction(alias or) : Instruction
+{
+private:
+    Parameter value;
+
+public:
+    this(size_t instructionPointer, int[] memory)
+    {
+        value = parameter(instructionPointer, memory, 0);
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        or.put(value.derefIfNeeded(memory));
+        instructionPointer += 2;
+        return Continue.yes;
+    }
+}
+
+class JumpIfTrueInstruction : Instruction
+{
+private:
+    Parameter toBeChecked, target;
+
+public:
+    this(size_t instructionPointer, int[] memory)
+    {
+        toBeChecked = parameter(instructionPointer, memory, 0);
+        target = parameter(instructionPointer, memory, 1);
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        if (toBeChecked.derefIfNeeded(memory) != 0)
+            instructionPointer = target.derefIfNeeded(memory).to!size_t;
+        else
+            instructionPointer += 3;
+        return Continue.yes;
+    }
+}
+
+class JumpIfFalseInstruction : Instruction
+{
+private:
+    Parameter toBeChecked, target;
+
+public:
+    this(size_t instructionPointer, int[] memory)
+    {
+        toBeChecked = parameter(instructionPointer, memory, 0);
+        target = parameter(instructionPointer, memory, 1);
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        if (toBeChecked.derefIfNeeded(memory) == 0)
+            instructionPointer = target.derefIfNeeded(memory).to!size_t;
+        else
+            instructionPointer += 3;
+        return Continue.yes;
+    }
+}
+
+class LessThanInstruction : Instruction
+{
+private:
+    Parameter value1, value2, outAddress;
+
+public:
+    this(size_t instructionPointer, int[] memory)
+    {
+        value1 = parameter(instructionPointer, memory, 0);
+        value2 = parameter(instructionPointer, memory, 1);
+        outAddress = parameter(instructionPointer, memory, 2);
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        outAddress.derefIfNeeded(memory) = value1.derefIfNeeded(
+                memory) < value2.derefIfNeeded(memory) ? 1 : 0;
+        instructionPointer += 4;
+        return Continue.yes;
+    }
+}
+
+class EqualsInstruction : Instruction
+{
+private:
+    Parameter value1, value2, outAddress;
+
+public:
+    this(size_t instructionPointer, int[] memory)
+    {
+        value1 = parameter(instructionPointer, memory, 0);
+        value2 = parameter(instructionPointer, memory, 1);
+        outAddress = parameter(instructionPointer, memory, 2);
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        outAddress.derefIfNeeded(memory) = value1.derefIfNeeded(
+                memory) == value2.derefIfNeeded(memory) ? 1 : 0;
+        instructionPointer += 4;
+        return Continue.yes;
+    }
+}
+
+class HaltInstruction : Instruction
+{
+    this(size_t instructionPointer, int[] memory)
+    {
+    }
+
+    override Continue execute(ref size_t instructionPointer, int[] memory)
+    {
+        return Continue.no;
+    }
+}
+
+Instruction nextInstruction(alias ir, alias or)(size_t instructionPointer, int[] memory)
+{
+    immutable opcode = memory[instructionPointer] % 100;
+    switch (opcode)
+    {
+    case Opcode.add:
+        return new AddInstruction(instructionPointer, memory);
+    case Opcode.multiply:
+        return new MultiplyInstruction(instructionPointer, memory);
+    case Opcode.input:
+        return new InputInstruction!(ir)(instructionPointer, memory);
+    case Opcode.output:
+        return new OutputInstruction!(or)(instructionPointer, memory);
+    case Opcode.jumpIfTrue:
+        return new JumpIfTrueInstruction(instructionPointer, memory);
+    case Opcode.jumpIfFalse:
+        return new JumpIfFalseInstruction(instructionPointer, memory);
+    case Opcode.lessThan:
+        return new LessThanInstruction(instructionPointer, memory);
+    case Opcode.equals:
+        return new EqualsInstruction(instructionPointer, memory);
+    case Opcode.halt:
+        return new HaltInstruction(instructionPointer, memory);
+    default:
+        throw new Exception("Invalid opcode %s".format(opcode));
+    }
+}
+
+void executeProgram(alias ir, alias or)(int[] memory)
+{
+    size_t instructionPointer = 0;
+    while (true)
+    {
+        auto instruction = nextInstruction!(ir, or)(instructionPointer, memory);
+        immutable _continue = instruction.execute(instructionPointer, memory);
+        if (_continue == Continue.no)
+            break;
+    }
+}
